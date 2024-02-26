6e0e90ff6a3e9130304dc76bff4c995ee839b2b5
tk-sawada <130136436+tk-sawada@users.noreply.github.com>
2024-02-26 13:12:06 +0900

    Add files via upload
    Added the .sh file.

---
+++ repairMacosClock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# timedプロセスのプロセスIDを取得
+TIMED_PID=$(ps -ef | grep '/usr/libexec/timed' | grep -v grep | awk '{print $2}')
+
+# timedプロセスが存在する場合は終了
+if [ ! -z "$TIMED_PID" ]; then
+    sudo kill "$TIMED_PID"
+fi
+
+# ntp.nict.jpに対してsntp -sSコマンドを実行
+sntp -sS ntp.nict.jp
