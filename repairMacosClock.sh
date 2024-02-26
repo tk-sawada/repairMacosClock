f064b5681931f5b17e1fa3505ef9ecf0c82000c2
tk-sawada <130136436+tk-sawada@users.noreply.github.com>
2024-02-26 20:20:57 +0900

    Update repairMacosClock.sh
    Removed "sudo" command.

--- repairMacosClock.sh
+++ repairMacosClock.sh
@@ -5,7 +5,7 @@ TIMED_PID=$(ps -ef | grep '/usr/libexec/timed' | grep -v grep | awk '{print $2}'
 
 # timedプロセスが存在する場合は終了
 if [ ! -z "$TIMED_PID" ]; then
-    sudo kill "$TIMED_PID"
+    kill "$TIMED_PID"
 fi
 
 # ntp.nict.jpに対してsntp -sSコマンドを実行
